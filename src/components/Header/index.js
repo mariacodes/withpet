6226db25fab6e056772d7d12f94c4077120724cf
mariacodes <maria.c.mury@gmail.com>
2021-09-23 21:46:28 -0400

    Header - completed the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,27 +1,56 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Nav, Container, Navbar } from 'react-bootstrap';
 
 const headerStyle = {
   textAlign: 'center', // text-align: 'center';
   top: 0,
-  width: '100%',
-  display: 'flex',
-  flexDirection: 'row', // flex-direction: 'row';
-  justifyContent: 'space-evenly',
+  left: 0,
+  position: 'relative',
+  fontFamily: 'Rubik',
+  fixed: 'top',
+  backgroundColor: '#F7F6F2',
+};
+
+const navbarbrand = {
+  paddingLeft: '8%',
+  textAlign: 'left',
+  fontFamily: 'Patrick Hand',
+  color: 'grey',
+  fontSize: '55px',
+  margin: '0px',
+};
+
+const leftStyle = {
+  paddingRight: '60px',
+};
+
+const imageIcon = {
+  width: '13%',
+  height: '13%',
+  padding: '0 10px 0 10px',
+  filter: 'sepia(40%)',
 };
 
 function Header() {
   return (
     <div style={headerStyle}>
-      <Link to="/">
-        <h1>Home</h1>
-      </Link>
-      <Link to="/destinations">
-        <h1>Destinations</h1>
-      </Link>
-      <Link to="/stays">
-        <h1>Stays</h1>
-      </Link>
+      <container>
+        <Navbar bg="light" variant="light">
+          <Navbar.Brand href="/" style={navbarbrand}>
+            <img src="/paw.png" style={imageIcon}></img>
+            With Pets
+          </Navbar.Brand>
+          <Nav className="ms-auto">
+            <Nav.Link href="/destinations" style={leftStyle}>
+              Destinations
+            </Nav.Link>
+            <Nav.Link href="/stays" style={leftStyle}>
+              Stays
+            </Nav.Link>
+          </Nav>
+        </Navbar>
+      </container>
     </div>
   );
 }
